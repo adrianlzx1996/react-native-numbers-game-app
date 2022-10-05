2dbe2858436423e76fe5de33aa4419840c700315
Adrian Leong <leongadrian36@gmail.com>
2022-10-05 15:02:58 +0800

    title component

---
+++ components/Title.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from "react-native";
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  title: {
+    padding: 12,
+    fontSize: 18,
+    borderWidth: 2,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    textAlign: "center",
+    borderColor: "#ddb52f",
+  },
+});
+
+export default Title;
