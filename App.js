ad36eeaa8367159ddb6bc2a35cfa7b5f5df5b182
Adrian Leong <leongadrian36@gmail.com>
2022-10-05 21:34:02 +0800

    use custom fonts

--- App.js
+++ App.js
@@ -5,54 +5,60 @@ import { useState } from "react";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
 import GameOverScreen from "./screens/GameOverScreen";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameIsOver, setGameIsOver] = useState(true);
+	const [userNumber, setUserNumber] = useState();
+	const [gameIsOver, setGameIsOver] = useState(true);
 
-  function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
-    setGameIsOver(false);
-  }
+	useFonts({
+		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+		"open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+	});
 
-  function gameOverHandler() {
-    setGameIsOver(true);
-  }
+	function pickedNumberHandler(pickedNumber) {
+		setUserNumber(pickedNumber);
+		setGameIsOver(false);
+	}
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+	function gameOverHandler() {
+		setGameIsOver(true);
+	}
 
-  if (userNumber) {
-    screen = (
-      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-    );
-  }
+	let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
 
-  if (userNumber && gameIsOver) {
-    screen = <GameOverScreen />;
-  }
+	if (userNumber) {
+		screen = (
+			<GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+		);
+	}
 
-  return (
-    <LinearGradient
-      colors={[Colors.primary700, Colors.accent500]}
-      style={styles.rootScreen}
-    >
-      <ImageBackground
-        source={require("./assets/images/background.png")}
-        resizeMode={"cover"}
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
-  );
+	if (userNumber && gameIsOver) {
+		screen = <GameOverScreen/>;
+	}
+
+	return (
+		<LinearGradient
+			colors={[Colors.primary700, Colors.accent500]}
+			style={styles.rootScreen}
+		>
+			<ImageBackground
+				source={require("./assets/images/background.png")}
+				resizeMode={"cover"}
+				style={styles.rootScreen}
+				imageStyle={styles.backgroundImage}
+			>
+				<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+			</ImageBackground>
+		</LinearGradient>
+	);
 }
 
 const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1,
-  },
-  backgroundImage: {
-    opacity: 0.2,
-  },
+	rootScreen: {
+		flex: 1,
+	},
+	backgroundImage: {
+		opacity: 0.2,
+	},
 });
