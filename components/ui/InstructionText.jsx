3ee9175e6197d320782a797346cd3293ba265884
Adrian Leong <leongadrian36@gmail.com>
2022-10-05 21:39:59 +0800

    use custom font

--- components/ui/InstructionText.jsx
+++ components/ui/InstructionText.jsx
@@ -1,15 +1,16 @@
 import { StyleSheet, Text } from "react-native";
 import Colors from "../../constants/colors";
 
-function InstructionText({ children, style }) {
-  return <Text style={[styles.instructionText, style]}>{children}</Text>;
+function InstructionText({children, style}) {
+	return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
-  instructionText: {
-    color: Colors.accent500,
-    fontSize: 24,
-  },
+	instructionText: {
+		color: Colors.accent500,
+		fontFamily: "open-sans",
+		fontSize: 24,
+	},
 });
 
 export default InstructionText;
--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,19 +1,19 @@
 import { StyleSheet, Text } from "react-native";
 
-function Title({ children }) {
-  return <Text style={styles.title}>{children}</Text>;
+function Title({children}) {
+	return <Text style={styles.title}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
-  title: {
-    padding: 12,
-    fontSize: 18,
-    borderWidth: 2,
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-    borderColor: "white",
-  },
+	title: {
+		padding: 12,
+		fontSize: 18,
+		borderWidth: 2,
+		color: "white",
+		textAlign: "center",
+		borderColor: "white",
+		fontFamily: "open-sans-bold",
+	},
 });
 
 export default Title;
